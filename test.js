e8bff7fef9fd57e10f3f5f9a5b6255eabfae3da1
freexiaochuan <w2239559319@myyahoo.com>
2026-01-21 07:59:49 +0000

    u

---
+++ test.js
@@ -0,0 +1,39 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const fs = require("fs");
+const path = require("path");
+
+puppeteer.use(StealthPlugin());
+
+async function getItems(page, url) {
+  await page.goto(url);
+  await page.waitForSelector(".generate-columns-container");
+
+  const itemElements = await page.$$(".generate-columns-container article");
+  const items = [];
+  console.log(itemElements);
+
+  return {
+    items,
+    nextPageUrl,
+  };
+}
+
+(async () => {
+  /**
+   * @type {import('puppeteer-core').Browser}
+   */
+  const browser = await puppeteer.launch({
+    channel: "chrome",
+    executablePath: '/usr/bin/chromium-browser', // 或 '/usr/bin/google-chrome'
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: 'new'
+    // headless: false
+  });
+  const page = await browser.newPage();
+  const res = [];
+
+  getItems(page, 'https://freemagazinespdf.com/');
+
+  await browser.close();
+})();
